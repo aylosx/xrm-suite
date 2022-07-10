0dd7cbe23ce1889065a8aa39b2a4d8156abcdb02
Vangelis Xanthakis <vangelis.xanthakis@aylos.com>
2022-07-10 10:50:46 +0100

    Added the backup version 2 script that supports backups by service principals

--- src/BuildTools/Scripts/Backup_v2.ps1
+++ src/BuildTools/Scripts/Backup_v2.ps1
@@ -3,8 +3,9 @@
 		.\Backup.ps1
 			-EnvironmentName "dbf06063-0009-4b43-92dd-f55a3ac5e2f1"
 			-BackupLabel "ReleaseName"
-			-Username "jsmith@aylos.com"
-			-SecurePassword "***********" or [-Password="passcode" -AsPlainText]
+			-TenantId "5bbf182b-e07d-4aa4-a752-094a5b8a019c"
+			-ServicePrincipalId "af3e5418-d265-40eb-995f-4586a29d5a89"
+			-SecureServicePrincipalSecret "***********" or [-ServicePrincipalSecret "secret" -AsPlainText]
 	#>
 
 [CmdletBinding(DefaultParameterSetName="AsEncryptedText")]
@@ -13,11 +14,12 @@ Param(
 	[parameter(Mandatory=$false)][String]$Endpoint = "prod",
 	[parameter(Mandatory=$true)][String]$EnvironmentName,
 	[parameter(Mandatory=$true)][String]$BackupLabel,
-	[parameter(Mandatory=$true)][String]$Username,
+	[parameter(Mandatory=$true)][String]$TenantId,
+	[parameter(Mandatory=$true)][String]$ServicePrincipalId,
 	[parameter(Mandatory=$false, ParameterSetName="AsEncryptedText")][switch]$AsEncryptedText,
-	[parameter(Mandatory=$true, ParameterSetName="AsEncryptedText")][SecureString]$SecurePassword,
+	[parameter(Mandatory=$true, ParameterSetName="AsEncryptedText")][SecureString]$SecureServicePrincipalSecret,
 	[parameter(Mandatory=$false, ParameterSetName="AsPlainText")][switch]$AsPlainText,
-	[parameter(Mandatory=$true, ParameterSetName="AsPlainText")][String]$Password
+	[parameter(Mandatory=$true, ParameterSetName="AsPlainText")][String]$ServicePrincipalSecret
 )
 
 Write-Host "*".PadRight($Host.UI.RawUI.WindowSize.Width, "*")
@@ -33,13 +35,12 @@ switch ($PSCmdlet.ParameterSetName)
 {
     "AsEncryptedText"
     {
-		$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
-		$Password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-		break
+		$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureServicePrincipalSecret)
+		$ServicePrincipalSecret = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
     }
 }
 
-Add-PowerAppsAccount -Endpoint "$Endpoint" -Username "$Username" -Password (ConvertTo-SecureString -String "$Password" -AsPlainText -Force)
+Add-PowerAppsAccount -Endpoint "$Endpoint" -TenantID "$TenantId" -ApplicationId "$ServicePrincipalId" -ClientSecret "$ServicePrincipalSecret" -Verbose
 
 Get-AdminPowerAppEnvironment -EnvironmentName $EnvironmentName
 
@@ -48,4 +49,3 @@ Get-PowerAppEnvironmentBackups -EnvironmentName $EnvironmentName
 if ((Get-PowerAppEnvironmentBackups -EnvironmentName $EnvironmentName) -contains "$BackupLabel") { Write-Host "A backup named $BackupLabel already exists!" }
 
 Backup-PowerAppEnvironment -EnvironmentName $EnvironmentName -BackupRequestDefinition ([pscustomobject]@{ Label = "$BackupLabel"; Notes = "This is an automated backup triggered by the release."})
-
